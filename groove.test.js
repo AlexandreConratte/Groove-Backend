286b490f77b00ed4ab3aa3cd09335a30b6ff0bbf
camille <camilleurb@live.fr>
2024-05-20 17:33:14 +0200

    add test Get users/findliked

--- groove.test.js
+++ groove.test.js
@@ -1,3 +1,10 @@
+const request = require('supertest');
+const app = require('./app');
+const mongoose = require('mongoose');
+const User = require('./models/users');
+
+const newUser = { username: 'test', email:'fake@email.com', password: 'test123' };
+
 it('POST /users', async () => {
     const res = await request(app).post('/users/signup').send({
       pseudo: 'Alex',
@@ -26,3 +33,26 @@ it('POST /users', async () => {
     expect(res.body.birthDa).toBe('');
     expect(res.body.city).toBe(''); 
    });
+
+
+
+it('Get /users/findLiked', async () => { 
+  const res = await request(app).post('/users/signup').send(newUser);
+  expect(res.statusCode).toBe(200);
+  expect(res.body.result).toBe(true);
+  expect(res.body.token).toEqual(expect.any(String));
+
+  const res2 = await request(app).get('festival/findPopular')
+  expect(res2.statusCode).toBe(200);
+
+  const res3 = await request(app).post('users/likeFest').send(res2.body.festivals[0]);
+  expect(res3.statusCode).toBe(200);
+
+  const res4 = await request(app).post('users/likeFest').send(res2.body.festivals[1]);
+  expect(res4.statusCode).toBe(200);
+
+  const res5 = await request(app).get(`users/findLiked/${res.body.token}`)
+  expect(res5.statusCode).toBe(200);
+  expect(res5.body.result).toBe(true);
+  expect(res5.body.festivals.length).toEqual(2);
+});
