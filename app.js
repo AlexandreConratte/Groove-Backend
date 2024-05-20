5247ae38f5cb8121eb24b48a8f3832b0b359f585
Alexandre Conratte <conrattealex@gmail.com>
2024-05-20 11:08:11 +0200

    sign up, modules checkbody , model users, modification app avec ajout cors/jest/routes/dotevn

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -5,8 +6,16 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var groupsRouter = require('./routes/groups');
+var artistsRouter = require('./routes/artists');
+var festivalsRouter = require('./routes/festivals');
+var stylesRouter = require('./routes/styles');
+
 
 var app = express();
+
+const cors = require('cors');
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/festivals', festivalsRouter);
+app.use('/artists', artistsRouter);
+app.use('/groups', groupsRouter);
+app.use('/styles', stylesRouter);
 
 module.exports = app;
