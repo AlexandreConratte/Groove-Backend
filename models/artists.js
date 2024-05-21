042fd609b4c188b6af9febdedfb74b1ff2136551
Louis Catteau <louiscatteauo@gmail.com>
2024-05-21 09:37:31 +0200

    fin de la route new festival

--- models/artists.js
+++ models/artists.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const artistSchema = mongoose.Schema({
     name: String,
     picture: String,
-    styles: [{ type: mongoose.Schema.Types.ObjectId, ref: 'styles' }],
 });
 
 const Artist = mongoose.model('artists', artistSchema);
--- routes/festivals.js
+++ routes/festivals.js
@@ -15,59 +15,64 @@ router.get('/findAll', function (req, res) {
 
 
 
-router.post('/newFestival', function (req, res) {
-  festivalsData.forEach((element) => {
-    Festival.findOne({ name: element.name })
-      .then((data) => {
-        if (!data) {
-          let [day, month, year] = element.start.split('/')
-          const start = new Date(+year, +month - 1, +day)
+router.post('/newFestival', async (req, res) => {
+  for (let element of festivalsData) {
+    let styles = [];
+    let artists = [];
 
-          let [day2, month2, year2] = element.end.split('/')
-          const end = new Date(+year2, +month2 - 1, +day2)
+    for (let style of element.styles) {
+      const data = await Style.findOne({ name: style })
 
-          let styles = []
-          let artists = []
+      if (data === null) {
+        const newStyle = new Style({
+          name: style
+        })
+        const saveStyle = await newStyle.save()
+        styles.push(saveStyle.id)
+      }
+      else {
+        styles.push(data.id)
+      }
+    }
 
-          element.styles.forEach((style) => {
-            Style.findOne({ name: style })
-              .then(data => {
-                if (data) {
-                  styles.push(data.id)
-                }
-                else {
-                  const newStyle = new Style({
-                    name: style
-                  })
-                  newStyle.save()
-                    .then(styles.push(newStyle.id))
-                }
-              })
-          })
+    for (let artist of element.artists) {
+      const data = await Artist.findOne({ name: artist })
 
-          const newFestival = new Festival({
-            name: element.name,
-            start: start,
-            end: end,
-            averageParticipant: element.averageParticipant,
-            description: element.description,
-            moreAbout: element.moreAbout,
-            pictures: [],
-            nbLikes: [],
-            adress: element.adress,
-            styles: styles,
-            artists: artists,
-          });
+      if (data === null) {
+        const newArtist = new Artist({
+          name: artist
+        })
+        const saveArtist = await newArtist.save()
+        artists.push(saveArtist.id)
+      }
+      else {
+        artists.push(data.id)
+      }
+    }
 
-          newFestival.save().then(newDoc => {
-            res.json({ result: true, token: newDoc.token });
-          });
-        }
-        else {
-          res.json({ result: false, message: 'festival already exist' })
-        }
-      })
-  })
-});
+    let [day, month, year] = element.start.split('/')
+    const start = new Date(+year, +month - 1, +day)
+
+    let [day2, month2, year2] = element.end.split('/')
+    const end = new Date(+year2, +month2 - 1, +day2)
+
+    const newFestival = new Festival({
+      name: element.name,
+      start: start,
+      end: end,
+      averageParticipant: element.averageParticipant,
+      description: element.description,
+      moreAbout: element.moreAbout,
+      pictures: element.pictures,
+      nbLikes: element.nbLikes,
+      adress: element.adress,
+      styles: styles,
+      artists: artists,
+    });
+    newFestival.save()
+  }
+  res.json({ result: true })
+})
+
 
 module.exports = router;
