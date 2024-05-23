fbda05f90dac6a8f36b2a7cea0c6fce130727da8
camille <camilleurb@live.fr>
2024-05-23 16:29:54 +0200

    route iprofil modified res.json

--- routes/users.js
+++ routes/users.js
@@ -183,13 +183,13 @@ router.post('/findMemories', (req,res) => {
 router.post('/iprofil', (req,res) => {
   const { token } = req.body
 
-  User.findOne({ token : token }).populate('styles','artists')
+  User.findOne({ token : token }).populate('styles').populate('artists')
   .then(user => {
     if(!user) {
       return res.json({ result: false, error: 'User not found' })
     }
 
-    res.json({ result: true, user})
+    res.json({ result: true, username: user.username, picture: user.picture, lastname: user.lastname, firstname: user.firstname, email: user.email, city: user.city, birthdate: user.birthdate, artists: user.artists, styles: user.styles})
   })
 })
 
