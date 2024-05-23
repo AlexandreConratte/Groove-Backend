b00eb1edddad78b229e8cb6148fe8e4866ad827d
Alexandre Conratte <conrattealex@gmail.com>
2024-05-23 16:04:37 +0200

    modifications schema

--- routes/users.js
+++ routes/users.js
@@ -2,48 +2,50 @@ var express = require('express');
 var router = express.Router();
 require('../models/connection');
 const User = require('../models/users');
-const Festival =require('../models/festivals');
+const Festival = require('../models/festivals');
 const { checkBody } = require('../modules/checkBody');
 const uid2 = require('uid2');
 const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
-const mailregex =  /^((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))$/
+const mailregex = /^((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))$/
 
 router.post('/signup', (req, res) => {
   if (!checkBody(req.body, ['username', 'email', 'password'])) {
     res.json({ result: false, error: 'Missing or empty fields' });
     return;
   }
-  User.findOne({ username: req.body.username , email : req.body.email}).then(data => {
+  User.findOne({ username: req.body.username, email: req.body.email }).then(data => {
     if (data === null) {
       const hash = bcrypt.hashSync(req.body.password, 10);
-    
+
       /* Création de userobligate, avec les champs obligatoires */
       const userObligate = {
         username: req.body.username,
         email: req.body.email,
         password: hash,
         token: uid2(32),
-      };   
-         /* création d'une const optional, qui va chercher la présence ou non des champs optionnels, et les ajouter à userObligate*/   
+      };
+      /* création d'une const optional, qui va chercher la présence ou non des champs optionnels, et les ajouter à userObligate*/
       const optionalFields = ['firstname', 'lastname', 'birthdate', 'city', 'styles', 'artists', 'friends', 'likedFestivals', 'memoriesFestivals', 'picture']
       optionalFields.forEach(field => {
         if (req.body[field]) {
           userObligate[field] = req.body[field];
-        }  
-      });  
-      console.log(userObligate)
+        }
+      });
+      // console.log(userObligate)
 
-      /* création du new user avec les champs obligatoires + ceux opitonnels trouvés dans la const userObligate */ 
+      /* création du new user avec les champs obligatoires + ceux opitonnels trouvés dans la const userObligate */
       const newUser = new User(userObligate);
 
       newUser.save().then(data => {
+
         res.json({ result: true, token: data.token });
       });
     } else {
@@ -61,23 +63,23 @@ router.post('/signin', (req, res) => {
 
   User.findOne({ username: req.body.username }).then(data => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
-      res.json({ result: true, username: data.username, token: data.token  });
+      res.json({ result: true, username: data.username, token: data.token });
     } else {
       res.json({ result: false, error: 'User not found or wrong password' });
     }
   });
 });
 
-router.post('/likeDislikeFestival', (req,res) => {
+router.post('/likeDislikeFestival', (req, res) => {
   const { festivalId, token } = req.body;
 
   User.findOne({ token: token }).then(user => {
     if (!user) {
       return res.json({ result: false, error: 'User not found' });
-    } 
+    }
 
     const index = user.likedFestivals.indexOf(festivalId);
-    
+
     Festival.findById(festivalId).then(festival => {
       if (!festival) {
         return res.json({ result: false, error: 'Festival not found' });
@@ -104,20 +106,43 @@ router.post('/likeDislikeFestival', (req,res) => {
   })
 })
 
-router.post('/findLiked', (req,res) => {
+router.post('/findLiked', (req, res) => {
   const { token } = req.body;
 
   User.findOne({ token: token }).populate('likedFestivals')
-  .then(user => {
-    if (!user) {
-      return res.json({ result: false, error: 'User not found' });
-    } 
+    .then(user => {
+      if (!user) {
+        return res.json({ result: false, error: 'User not found' });
+      }
 
-    res.json({result: true, festivalsLiked: user.likedFestivals})
+      res.json({ result: true, festivalsLiked: user.likedFestivals })
 
-  })
+    })
 })
 
+router.post('/checkUser', (req, res) => {
+  const { username } = req.body;
+  User.findOne({ username })
+  .then( data => {
+    if (data) {
+    res.json({ result: true, error: "User déjà existant" }); //verifie qu'il y a un utilisateur, ce qui nous coduit à l'erreur en screen connect2
+  } else {
+    res.json({ result: false });
+  }
+})
+})
+
+router.post('/checkMail', (req, res) => {
+  const { email } = req.body;
+  User.findOne({ email })
+  .then( data => {
+  if (data) {
+    res.json({ result: true, error: "Mail déjà existant" });
+  } else {
+    res.json({ result: false });
+  }
+})
+})
 
 
 
