b975485b84b4a2ead623548e2d402318cdb5cbc3
Louis Catteau <louiscatteauo@gmail.com>
2024-05-24 12:34:11 +0200

    ajout nouvelles routes users

--- routes/users.js
+++ routes/users.js
@@ -13,9 +13,26 @@ const fs = require('fs');
 
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
+router.get('/findAll', function (req, res) {
+  User.find()
+    .then(data => (res.json({ result: true, users: data })))
 });
+
+router.post('/getAllFriends', function (req, res) {
+  User.find({token:req.body.token})
+    .then(data => (res.json({ result: true, friends: data.friends })))
+});
+
+router.put('/addFriend', function (req, res) {
+  User.findOne({ token: req.body.token })
+  .then(data => {
+    const newFriend = [...data.friends, req.body.user]
+    User.updateOne({ token: req.body.token }, { friends: newFriend })
+      .then(() => res.json({ result: true, message: 'Ami(e) ajouté' }))
+  })
+})
+
+
 
 const mailregex = /^((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))$/
 
