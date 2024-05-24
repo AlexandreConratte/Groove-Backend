0d9146b078a55ecf6c87985b74c21d6d7b325b4a
Louis Catteau <louiscatteauo@gmail.com>
2024-05-24 11:18:44 +0200

    ajout des routes groups

--- routes/groups.js
+++ routes/groups.js
@@ -4,9 +4,40 @@ require('../models/connection');
 
 const Group = require('../models/groups');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/findAll', function (req, res) {
+  Group.find()
+    .then(data => (res.json({ result: true, groups: data })))
 });
 
+router.post('/newGroup', function (req, res) {
+  Group.findOne({ name: req.body.name })
+    .then(data => {
+      if (data) {
+        res.json({ result: false, message: 'Le groupe existe déjà' })
+      }
+      else {
+        const newGroup = new Group({
+          name: req.body.name,
+          festival: req.body.festival,
+          members: req.body.user
+        });
+        newGroup.save().then(data => {
+          res.json({ result: true, group: data });
+        });
+      }
+    })
+})
+router.put('/newUser', function (req, res) {
+  Group.findOne({ name: req.body.name })
+    .then(data => {
+      const newUser = [...data.members, req.body.user]
+      Group.updateOne({ name: req.body.name }, { members: newUser })
+        .then(() => res.json({ result: true, message: 'Utilisateur ajouté' }))
+    })
+})
+
+router.delete('/deleteGroup', function (req, res) {
+  Group.deleteOne({ name: req.body.name })
+  .then(() => res.json({ result: true,message: 'Groupe supprimé'}));
+})
 module.exports = router;
