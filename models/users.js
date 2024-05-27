83e662a633f2f3889506f3e3adeabde5822b5619
camille <camilleurb@live.fr>
2024-05-27 11:32:30 +0200

    add settings in userSchema

--- models/users.js
+++ models/users.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
+const settingSchema = mongoose.Schema({
+    nightMode: { type : Boolean, default : false },
+    notifications: { type: Boolean, default : false },
+})
+
 const userSchema = mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
@@ -15,8 +20,8 @@ const userSchema = mongoose.Schema({
     friends: { type: [mongoose.Schema.Types.ObjectId], ref: 'users', default: [] },
     likedFestivals: { type: [mongoose.Schema.Types.ObjectId], ref: 'festivals', default: [] },
     memoriesFestivals: { type: [mongoose.Schema.Types.ObjectId], ref: 'festivals', default: [] },
-    picture: { type : String, default : null }
-
+    picture: { type : String, default : null },
+    settings: settingSchema
 });
 
 const User = mongoose.model('users', userSchema);
