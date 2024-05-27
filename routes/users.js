915f209bce5de6a7d2c6b8e3707db61c2715d988
Louis Catteau <louiscatteauo@gmail.com>
2024-05-27 13:01:57 +0200

    modification des routes deletefriend et add friend

--- routes/users.js
+++ routes/users.js
@@ -35,30 +35,20 @@ router.post('/getAllFriends', function (req, res) {
 });
 
 router.put('/addFriend', function (req, res) {
-  User.findOne({ token: req.body.token })
-    .then((data) => {
-      User.findOne({ token: req.body.friendToken })
-        .then((friendData) => {
-          const newFriend = [...data.friends, friendData.id]
-          User.updateOne({ token: req.body.token }, { friends: newFriend })
-            .then(() => res.json({ result: true, message: 'Ami(e) ajouté' }))
-        })
+  User.findOne({ token: req.body.friendToken })
+    .then((friendData) => {
+      User.updateOne({ token: req.body.token }, { $push: { friends: friendData.id } })
+        .then(() => res.json({ result: true, message: 'Ami(e) ajouté' }))
     })
 })
 
 router.put('/deleteFriend', function (req, res) {
-  User.findOne({ token: req.body.token })
-    .then((data) => {
-      User.findOne({ token: req.body.friendToken })
-        .then((friendData) => {
-          const newtab = data.friends.filter((e) => e !== friendData.id)
-          User.updateOne({ token: req.body.token }, { friends: newtab })
-            .then(() => res.json({ result: true, message: 'Ami(e) supprimé' }))
-        })
+  User.findOne({ token: req.body.friendToken })
+    .then((friendData) => {
+      User.updateOne({ token: req.body.token }, { $pull: { friends: friendData.id } })
+        .then(() => res.json({ result: true }))
     })
 })
-
-
 
 const mailregex = /^((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))$/
 
