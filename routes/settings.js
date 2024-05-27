07867ea22796483f8bd29d959a53fa11afa8268c
camille <camilleurb@live.fr>
2024-05-27 12:20:25 +0200

    modification route settings mode

--- routes/settings.js
+++ routes/settings.js
@@ -6,23 +6,27 @@ const User = require('../models/users');
 
 
 router.put('/mode', function (req, res) {
-    const { token } = req.body;
+    const { token, nightMode } = req.body;
+
+    if (nightMode !== undefined && typeof nightMode !== 'boolean') {
+        return res.json({ result: false, error: 'Invalid value for nightMode' });
+    }
 
     User.findOne({ token: token })
         .then(user => {
-            if (!user) {
+            if (!user || !token) {
                 return res.json({ result: false, error: 'User not found' });
             }
 
-            user.settings.nightMode = !user.settings.nightMode;
+            // Si nightMode est défini et a une valeur valide, alors on met à jour la valeur dans la base de données
+            if (nightMode !== undefined) {
+                user.settings.nightMode = nightMode;
+            }
 
             user.save()
                 .then(updatedUser => {
                     res.json({ result: true, nightMode: updatedUser.settings.nightMode });
                 })
-                .catch(error => {
-                    res.json({ result: false, error: 'Error updating user' });
-                });
         })
 });
 
