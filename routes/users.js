d1fe586f343ab3d474e09475b8140fb346a49405
Alex <conrattealex@gmail.com>
2024-05-28 11:06:05 +0200

    tentative route photo

--- routes/users.js
+++ routes/users.js
@@ -12,8 +12,8 @@ const cloudinary = require('cloudinary').v2;
 const fs = require('fs');
 import path from 'path';
 // const express = require('express')  
-// const fileUpload = require('express-fileupload');
-const app = express();
+const fileUpload = require('express-fileupload');
+
 
 router.post('/infoUser', (req, res) => {
   User.findById(req.body.id)
@@ -302,7 +302,7 @@ router.put('/update', (req, res) => {
 
 
 
- // app.use(fileUpload())
+ app.use(fileUpload())
 
 // Configurer Cloudinary
 cloudinary.config({
